505b3484888eb65899756d5cf1dba053a02447c2
aiien61 <aiien61will@gmail.com>
2024-04-24 14:42:41 +0800

    regular expression tasks

--- challenge/regrex_to_strip.py
+++ challenge/regrex_to_strip.py
@@ -10,18 +10,34 @@ def strip_by_regrex(*chars) -> str:
         return strip_regrex.sub('', chars[0])
     else:
         lstrip_regrex = re.compile(r'^' + chars[1] + r'*([.]{0,1})')
-        rstrip_regrex = re.compile(r'([.]{0,1}' + chars[1] + r'*$)')
+        rstrip_regrex = re.compile(r'([.]{0,1})' + chars[1] + r'*$')
         result = pipe(chars[0],
-                      lambda string: lstrip_regrex.sub('\1', string),
-                      lambda string: rstrip_regrex.sub('\1', string))
+                      lambda string: lstrip_regrex.sub(r'\1', string),
+                      lambda string: rstrip_regrex.sub(r'\1', string))
         return result
     
+    
 class Test(unittest.TestCase):
-    strings = ['---xy-yx---']
+    strings = {
+        '   xy-yx   ': None,
+        '---xy-yx---': '-',
+        'xy-yx------': '-',
+        '------xy-yx': '-',
+        'xy---yx': None, 
+        '       ': None,
+        'xy---yx': '-'
+    }
+
+    def test_regrex(self):
+        for string, trailer in self.strings.items():
+            if not trailer:
+                expected = string.strip()
+                actual = strip_by_regrex(string)
+            else:
+                expected = string.strip(trailer)
+                actual = strip_by_regrex(string, trailer)
+            self.assertEqual(actual, expected)
     
 
 if __name__ == "__main__":
-    print('---xy-yx---'.strip('-'))
-    # print(re.compile(r'^-*([.]{0,1})').sub('\1', '---xy-x--'))
-    # print(re.compile(r'([.]{0,1})-*$').sub('\1', '---xy-yx--'))
-    print(strip_by_regrex('         '))
+    unittest.main()
